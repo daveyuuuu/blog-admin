9dafff5d2c75b170afd74f55ed95a78f6558b45d
daveyu <daveyuuuu@gmail.com>
2022-01-27 14:39:58 +0800

    style: update .editorconfig, add stylelint, override stylelint eslint config with prettier

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  endOfLine: 'lf',
+};
