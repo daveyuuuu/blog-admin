79d836a1fa05dbc7adfe8cb87b7d6f79de10dfa8
daveyu <daveyuuuu@gmail.com>
2022-02-08 23:44:21 +0800

    feat(blog): add markdown editor

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,18 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/login'
+    redirect: '/blog'
   },
   {
     path: '/login',
     name: 'Login',
     component: () => import('@/views/login/Login.vue'),
   },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: () => import('@/views/blog/Blog.vue'),
+  }
 ];
 
 const router = createRouter({
---
+++ src/views/blog/Blog.vue
@@ -0,0 +1,18 @@
+<template>
+  <md-editor v-model="text" />
+</template>
+
+<script lang="ts">
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: { MdEditor },
+  data() {
+    return {
+      text: '',
+    };
+  },
+});
+</script>
